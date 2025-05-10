c6b842104b94e82fcc0c17b31f160d70d93edc41
Victor || VickyJay <77025670+Victorola-coder@users.noreply.github.com>
2025-05-10 01:01:28 +0100

    Update redirect path in Home component, remove unused user layout and home page files, add Splash component export, and delete unused brand images.

--- app/components/global/index.ts
+++ app/components/global/index.ts
@@ -2,6 +2,7 @@ export { default as AOS } from "./aos";
 export { default as AIO } from "./aio";
 export { default as Glow } from "./glow";
 export { default as Loader } from "./loader";
+export { default as Splash } from "./splash";
 export { default as Animation } from "./animation";
 export { default as ThemeToggle } from "./theme-toggle";
 export { ThemeProvider, useTheme } from "./theme-provider";
---
+++ app/components/global/splash.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Image } from "../ui";
+import { useTheme } from "./theme-provider";
+import { useState, useEffect } from "react";
+
+export default function Splash() {
+  const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // Avoid hydration mismatch
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  const logoSrc =
+    theme === "dark" ? "/images/brand/light.png" : "/images/brand/dark.png";
+
+  return (
+    <div className="w-full h-svh flex relative flex-col gap-5 items-center justify-center bg-gradient-to-r from-[#530CE2] to-[#17A3DA] text-white">
+      <Image src={logoSrc} alt="Tekcify" width={100} height={100} />
+      <h1 className="text-[16px] font-aki font-extrabold block">Tekcify</h1>
+    </div>
+  );
+}
